574d7360284804124fe3001330ec252c765e3101
Savanna Kemp <savannakemp@Savannas-MacBook-Pro.local>
2018-10-16 14:29:11 -0600

    done

--- practice.js
+++ practice.js
@@ -15,6 +15,12 @@
 */
 
 //Code here
+var me = {
+  name: 'savanna',
+  age: 24
+}
+alert(me.name);
+
 
 
 
@@ -26,24 +32,36 @@
 */
 
 //Code here
+var favoriteThings = {
+  band: 'Kygo',
+  food: 'Italian',
+  person: 'Josh',
+  book: 'Nocturnal Animals',
+  movie: 'Lion King',
+  holiday: 'New Years'
+};
+
+
+/*
+  After you've made your object, add another key named 'car' with the value being your favorite car and then another 
+  key named 'brand' with the value being your favorite brand.
+*/
+
+//Code here
+favoriteThings.car = 'Mercedez';
+favoriteThings.brand = 'G-Wagon';
 
 
 
 /*
-  After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
+  Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book 
+  key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
 
-
-
-/*
-  Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
-*/
-
-//Code here
-
-
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 ////////// PROBLEM 3 //////////
 
@@ -56,15 +74,19 @@
 */
 
 //Code here
+backPack = {
 
-
+}
+var item = 'firstPocket';
+backPack[item] = 'chapstick';
+backPack.color = 'black';
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+alert(backPack);
 
 
 /*
@@ -75,7 +97,7 @@ Instead, console.log your whole backPack object and then check out the console.
 
 //Code here
 
-
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
@@ -97,7 +119,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
